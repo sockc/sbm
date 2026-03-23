2614c978ec1d57242334cf3d3de747a4e664afba
sockc <86035855+sockc@users.noreply.github.com>
2026-03-24 06:47:37 +0800

    Update env.sh

--- lib/env.sh
+++ lib/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SBM_VERSION="0.1.4"
+SBM_VERSION="0.1.5"
 DEFAULT_SINGBOX_VERSION="1.13.3"
 
 PROJECT_NAME="sbm"
@@ -8,5 +8,7 @@ BASE_DIR="/usr/local/share/sbm"
 CONFIG_DIR="/etc/sing-box"
 BACKUP_DIR="${CONFIG_DIR}/backup"
 TMP_DIR="/tmp/sbm"
+SOURCES_DIR="${CONFIG_DIR}/sources"
+NODE_CACHE_DIR="${CONFIG_DIR}/node-cache"
 META_FILE="${CONFIG_DIR}/reality-meta.json"
 DEFAULT_CLIENT_FP="chrome"
