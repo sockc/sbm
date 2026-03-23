05013346a7b59d4a98e9e623d22436ed36daa9c3
sockc <86035855+sockc@users.noreply.github.com>
2026-03-24 03:31:33 +0800

    Create env.sh

---
+++ lib/env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SBM_VERSION="0.1.0"
+DEFAULT_SINGBOX_VERSION="1.13.3"
+
+PROJECT_NAME="sbm"
+BASE_DIR="/usr/local/share/sbm"
+CONFIG_DIR="/etc/sing-box"
+BACKUP_DIR="${CONFIG_DIR}/backup"
+TMP_DIR="/tmp/sbm"
